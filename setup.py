5f425661901d0ec4ba63ab5f5c57adf8790aee17
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2014-06-23 10:25:25 -0400

    Rename to sass.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-foundation',
+    name='django-foundation-sass',
     version='0.1',
     description='The Zurb Foundation responsive framework, along with templates and useful tags.',
     long_description=open('README.md').read(),
     author='Silvio J. Gutierrez',
     author_email='silviogutierrez@gmail.com',
-    url='https://github.com/silviogutierrez/django-foundation-scss',
+    url='https://github.com/silviogutierrez/django-foundation-sass',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
